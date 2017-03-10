847f2ca26670f3ff22fa38e52b7312875b8933d9
Juha Kaskiharju <juha.kaskiharju@gmail.com>
2017-03-10 18:39:50 +0200

    comment added to index.php

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
+/*
+
+This script finds all the .txt files in directory and finds every line which has variable end_date and changes 
+the date to something a modder has chosen for. This was important because i wanted to play Victoria 2 longer 
+than to the original end date of 1936 so i needed to change the end date for parties to new end date or otherwise 
+they would disappear in 1936
+
+*/
+
 if(isset($_GET['end']) && !empty($_GET['end'])) {
 	$end_date = $_GET['end'];
 } else {
